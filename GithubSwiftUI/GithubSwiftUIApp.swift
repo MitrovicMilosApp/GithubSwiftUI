004ec8a4b0af5bfe75ab804d3fedd4f209041052
Milos Mitrovic <mitrovic.milos.app@gmail.com>
2024-03-11 19:06:49 +0100

    Initial Commit

---
+++ GithubSwiftUI/GithubSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSwiftUIApp.swift
+//  GithubSwiftUI
+//
+//  Created by Milos Mitrovic on 11.3.24..
+//
+
+import SwiftUI
+
+@main
+struct GithubSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
