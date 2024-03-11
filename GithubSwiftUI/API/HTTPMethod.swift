d0b1bcbde9d683c2b71af8542157e675678ee307
Milos Mitrovic <mitrovic.milos.app@gmail.com>
2024-03-11 21:09:02 +0100

    Add infrastructure for API services (http method, api error, request provider, path provider)

---
+++ GithubSwiftUI/API/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  GithubSwiftUI
+//
+//  Created by Milos Mitrovic on 11.3.24..
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get     = "GET"
+    case post    = "POST"
+    case put     = "PUT"
+    case delete  = "DELETE"
+}
